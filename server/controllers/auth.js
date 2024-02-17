80eac6e24928570e2965564318eacb9920e03b12
Daniel <dani.marinescu7@gmail.com>
2024-02-17 08:04:55 +0200

    register users api working

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,41 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+        const {
+            username,
+            email,
+            password,
+            picturePath,
+            friends,
+            posts,
+            location,
+            occupation,
+            favoriteTeam
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            posts,
+            location,
+            occupation,
+            favoriteTeam,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
